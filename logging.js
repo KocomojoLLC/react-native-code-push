4a71e5b64380445659618505d5faba5ca27faaad
Elijah Windsor <ewindsor@gmail.com>
2017-02-19 21:57:51 -0500

    Removed logging

--- logging.js
+++ logging.js
@@ -1,6 +1,6 @@
 /* Logs messages to console with the [CodePush] prefix */
 function log(message) {
-  console.log(`[CodePush] ${message}`);
+//  console.log(`[CodePush] ${message}`);
 }
 
 module.exports = log;
